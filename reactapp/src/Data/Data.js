64da294a52773bd18cbfbe17cb53f30a1186adc3
Iamneo <techadmin@iamneo.ai>
2023-07-31 13:38:56 +0000

    Backup Commit

---
+++ reactapp/src/Data/Data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id: 0,
+        question: 'Who is the father of our nation ?',
+        options:{
+            option1: 'Mahatma Gandhi',
+            option2: 'Jawaharlal Nehru',
+            option3: 'Donald Trump',
+            option4: 'Barrack Obama',
+        },
+        answer:'Mahatma Gandhi',
+    },
+    {
+        id: 1,
+        question: 'What color is are the leaves ?',
+        options:{
+            option1: 'Blue',
+            option2: 'Red',
+            option3: 'Yellow',
+            option4: 'Green',
+        },
+        answer:'Green',
+    },
+    {
+        id: 2,
+        question: 'What color is the sky ?',
+        options:{
+            option1: 'Blue',
+            option2: 'Red',
+            option3: 'Yellow',
+            option4: 'Green',
+        },
+        answer:'Blue',
+    },
+    {
+        id: 3,
+        question: 'What color is the sky ?',
+        options:{
+            option1: 'Blue',
+            option2: 'Red',
+            option3: 'Yellow',
+            option4: 'Green',
+        },
+        answer:'Blue'
+    },
+    {
+        id: 4,
+        question: 'What color is are the fire ?',
+        options:{
+            option1: 'Blue',
+            option2: 'Red',
+            option3: 'Yellow',
+            option4: 'Green',
+        },
+        answer:'Red',
+    }
+]
+
+export default data;
