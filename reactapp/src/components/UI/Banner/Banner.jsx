0ccd7abbc515fe3e6c40f234ae920854a7c3067b
Iamneo <techadmin@iamneo.ai>
2023-07-31 15:08:16 +0000

    Backup Commit

--- reactapp/src/components/UI/Banner/Banner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Banner.css'
-
-export default function Banner(props) {
-    return <h4 className="banner">
-        You have answered {props.ans} / 5 Correctly
-    </h4>
-}
