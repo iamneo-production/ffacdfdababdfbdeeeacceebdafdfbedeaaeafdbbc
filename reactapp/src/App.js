7ddb416f4d4bbcd1d0f82c4a6770a8c2f0c72597
Iamneo <techadmin@iamneo.ai>
2023-07-31 17:35:10 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,65 +1,132 @@
-// import logo from './logo.svg';
-import './App.css';
-import Card from './components/UI/Card/Card';
-import data from './Data/Data.js'
-import Button from './components/UI/Button/Button';
-import Banner from './components/UI/Input/Input'
-import { useState } from 'react';
+import React from "react";
+import Data from "./Data.js";
+import Results from "./components/UI/Results";
+import Button from "./components/UI/Button/Button.js";
+import Card from "./components/UI/Card/Card.js";
+import Input from "./Input.js";
 
-function App() {
-  const [res, setRes] = useState(0);
-  const [btns, setBtns] = useState(0);
-  console.log(res);
-  const [show, setShow] = useState(false);
-  const [result, setResult] = useState('Show Results')
-  const [start, setStart] = useState(false);
-  
-  function handleStart() {
-    setStart(true);
-    document.getElementById('toggle').style.display = 'none';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAnswers: [],
+      disabledQuestions: [],
+      showResults: false,
+      showQuestions: false,
+      questionsCorrect: 0,
+      afterAnswerSubmit: false,
+    };
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+    this.handleShowResults = this.handleShowResults.bind(this);
+    this.handleResetQuiz = this.handleResetQuiz.bind(this);
+    this.handleStartQuiz = this.handleStartQuiz.bind(this);
+    this.calculateScore = this.calculateScore.bind(this);
+    this.renderResults = this.renderResults.bind(this);
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="title">Quizz App</h1>
+  handleAnswerSelected(questionIndex, answerIndex) {
+    const updatedSelectedAnswers = [...this.state.selectedAnswers];
+    updatedSelectedAnswers[questionIndex] = answerIndex;
+    this.setState({
+      selectedAnswers: updatedSelectedAnswers,
+    });
+    const updatedDisabledQuestions = [...this.state.disabledQuestions];
+    updatedDisabledQuestions[questionIndex] = true;
+    this.setState({
+      disabledQuestions: updatedDisabledQuestions,
+    });
+  }
 
-        <button id='toggle' onClick={handleStart}>Start Quiz</button>
+  handleShowResults() {
+    const res = this.calculateScore();
+    this.setState({
+      showResults: true,
+      showQuestions: false,
+      afterAnswerSubmit: true,
+      questionsCorrect: res
+    });
+  }
 
-        {start && 
-        <div className="gridcontainer">
-        {data.map(item => {
-          return <Card 
-            key={item['id']}
-            id={item['id']}
-            question={item['question']}
-            options={item['options']}
-            answer={item['answer']}
-            setRes={setRes}
-            res={res}
-            btns={btns}
-            setBtns={setBtns}
-            setStart={setStart}
-          />
-        })}
-        </div>
-        } 
-      </header>
-      {show && <Banner ans={res}/>}
-      {btns === 5  &&
-      <Button
-        setShow={setShow}
-        setResult={setResult}
-        result={result}
-        text={result}
-        option='a'
-        answer='b'
-        setRes={setRes}
-        setStart={setStart}
-      />
+  handleResetQuiz() {
+    this.setState({
+      selectedAnswers: [],
+      disabledQuestions: [],
+      showResults: false,
+      showQuestions: true,
+      questionsCorrect: 0,
+      afterAnswerSubmit: false,
+    });
+  }
+
+  calculateScore() {
+    let score = 0;
+    for (let i = 0; i < questionsData.length; i++) {
+      if (this.state.selectedAnswers[i] === questionsData[i].correctAnswer) {
+        score++;
       }
-    </div>
-  );
+    }
+    return score;
+  }
+
+  renderResults() {
+    const score = this.calculateScore();
+    return (
+      <Results
+        score={score}
+        numQuestions={questionsData.length}
+        onResetQuiz={this.handleResetQuiz}
+      />
+    );
+  }
+
+  handleStartQuiz() {
+    this.setState({
+      showQuestions: true,
+      afterAnswerSubmit: false,
+    });
+  }
+
+  render() {
+    const allQuestionsAnswered =
+      this.state.selectedAnswers.length === questionsData.length;
+
+    return (
+      <div className="App">
+        {!this.state.afterAnswerSubmit && <h1>Quizz App</h1>}
+        {!this.state.showQuestions && !this.state.afterAnswerSubmit && (
+          <Button onClick={this.handleStartQuiz}>Start Quiz</Button>
+        )}
+        {this.state.showQuestions &&
+          questionsData.map((question, index) => (
+            <Card
+              key={index}
+              question={question.question}
+              options={question.answers}
+              questionIndex={index}
+              selectedAnswer={this.state.selectedAnswers[index]}
+              onAnswerSelected={(answer) =>
+                this.handleAnswerSelected(index, answer)
+              }
+              disabled={this.state.disabledQuestions[index]}
+            />
+          ))}
+        {this.state.showQuestions && allQuestionsAnswered && !this.state.showResults && (
+          <Button onClick={this.handleShowResults} disabled={!allQuestionsAnswered}>
+            Show Results
+          </Button>
+        )}
+        {this.state.showResults && (
+          <>
+            <Input
+              questionsCorrect={this.state.questionsCorrect}
+              numQuestions={questionsData.length}
+            />
+            {this.renderResults()}
+          </>
+        )}
+      </div>)
+  }
+
 }
 
 export default App;
